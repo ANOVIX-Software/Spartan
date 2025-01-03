e94d8b7dc4fcfca3ecb11b47d559d180aff5f22d
nhuesman1043 <noah.huesman@gmail.com>
2025-01-02 20:17:06 -0600

    init: initial commit

---
+++ commitlint.config.ts
@@ -0,0 +1,29 @@
+import { UserConfig } from "@commitlint/types"
+
+const config: UserConfig = {
+	extends: ["@commitlint/config-conventional"],
+	parserPreset: "conventional-changelog-atom",
+	formatter: "@commitlint/format",
+	rules: {
+		// Ensures the subject is in lowercase
+		"subject-case": [2, "always", "lower-case"],
+		"type-enum": [
+			2,
+			"always",
+			[
+				"feat",
+				"fix",
+				"docs",
+				"style",
+				"refactor",
+				"perf",
+				"test",
+				"chore",
+				"revert",
+				"init",
+			],
+		],
+	},
+}
+
+export default config
